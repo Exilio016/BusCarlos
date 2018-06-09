858cab2d396e7f456e284d4552d1690666d1ad5e
Bruno Waldvogel <Wald@MacBook-Pro-de-Tomas.local>
2018-06-09 16:40:19 -0300

    Added routes and simplified ejs files for handling background variables

--- app.js
+++ app.js
@@ -1,21 +1,74 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
+const linhasRouter = express.Router();
+const referenciasRouter = express.Router();
+const favoritosRouter = express.Router();
+const feedRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
-app.use('/js', express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
-app.use('/js', express.static(path.join(__dirname, "/node_modules/jquery/dist/")));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.use('/linhas', linhasRouter);
+app.use('/referencias', referenciasRouter);
+app.use('/favoritos', favoritosRouter);
+app.use('/feed', feedRouter);
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
-})
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.listen(3000, function(){
-    debug('listening on port 3000');
+linhasRouter.route('/').get((req, res) => {
+  res.render(
+    'linhas',
+    {
+      nav: [{ link: '/linhas', title: 'Busca por Linhas' },
+        { link: '/referencias', title: 'Busca por Referencia' },
+        { link: '/preferidos', title: 'Favoritos' },
+        { link: 'feed', title: 'Feed de Noticias' }],
+    },
+  );
 });
+
+referenciasRouter.route('/').get((req, res) => {
+  res.render(
+    'referencias',
+    {
+      nav: [{ link: '/linhas', title: 'Busca por Linhas' },
+        { link: '/referencias', title: 'Busca por Referencia' },
+        { link: '/favoritos', title: 'Favoritos' },
+        { link: 'feed', title: 'Feed de Noticias' }],
+    },
+  );
+});
+
+favoritosRouter.route('/').get((req, res) => {
+  res.render(
+    'favoritos',
+    {
+      nav: [{ link: '/linhas', title: 'Busca por Linhas' },
+        { link: '/referencias', title: 'Busca por Referencia' },
+        { link: '/favoritos', title: 'Favoritos' },
+        { link: 'feed', title: 'Feed de Noticias' }],
+    },
+  );
+});
+
+feedRouter.route('/').get((req, res) => {
+  res.render(
+    'feed',
+    {
+      nav: [{ link: '/linhas', title: 'Busca por Linhas' },
+        { link: '/referencias', title: 'Busca por Referencia' },
+        { link: '/preferidos', title: 'Favoritos' },
+        { link: 'feed', title: 'Feed de Noticias' }],
+    },
+  );
+});
+
+app.listen(port, () => debug('listening on port '.concat(port)));
